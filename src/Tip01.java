76baebf25d1c39f8c1a00aa22f796089bdc00107
claudneia <164636272+claudneia@users.noreply.github.com>
2024-06-12 21:38:25 -0300

    Add files via upload
    Lição 4 Exercício 01 Parte02

---
+++ src/Tip01.java
@@ -0,0 +1,21 @@
+public class Tip01 {
+    public static void main(String[] args) {
+        //Find everyone's indivudlal total after tax(5%) and tip(15%)
+        double person1 = 10, person2 = 12, person3 = 9, person4 = 8, person5 = 7, person6 = 15, person7 = 11, person8 = 30;
+        //This is what everyone owes before tax and tip:
+        System.out.println(person1+(person1*0.20));
+        System.out.println(person2+(person2*0.20));
+        System.out.println(person3+(person3*0.20));
+        System.out.println(person4+(person4*0.20));
+        System.out.println(person5+(person5*0.20));
+        System.out.println(person6+(person6*0.20));
+        System.out.println(person7+(person7*0.20));
+        System.out.println(person8+(person8*0.20));
+        
+        
+        
+        
+        
+        
+    }    
+}
